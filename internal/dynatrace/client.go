7891825e999bcf6e46f5fd3cf8f0452c17c3e474
jose.carreno <jose.carreno@sap.com>
2024-02-20 14:01:15 +0100

    add dynatrace api client

---
+++ internal/dynatrace/client.go
@@ -0,0 +1,111 @@
+package dynatrace
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+const ConfigurationApiPath string = "/api/config/v1/"
+
+type DynatraceClient struct {
+	ApiToken string
+	EnvUrl   string
+	Client   *http.Client
+}
+
+type GetExtensionsResponse struct {
+	Extensions   []DynatraceExtensionInfo `json:"extensions"`
+	TotalResults int                      `json:"totalResults"`
+	NextPageKey  string                   `json:"nextPageKey"`
+}
+
+type DynatraceExtensionInfo struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func NewClient(envUrl *string, apiToken *string) (*DynatraceClient, error) {
+
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	client := DynatraceClient{
+		Client: &httpClient,
+	}
+
+	if envUrl == nil {
+		return nil, errors.New("environment URL is missing")
+	}
+
+	if apiToken == nil {
+		return nil, errors.New("API Token is missing")
+	}
+
+	client.ApiToken = *apiToken
+	client.EnvUrl = *envUrl
+
+	err := client.validateConnection()
+	if(err != nil) {
+		return nil, err
+	}
+
+	return &client, nil
+}
+
+func (c *DynatraceClient) validateConnection() error {
+	_, err := c.getExtensions(10, nil)
+	return err
+}
+
+func (c *DynatraceClient) getExtensions(pageSize int, nextPageKey *string) (*GetExtensionsResponse, error) {
+	req, err := c.getConfigurationApiRequest("GET", "/extensions")
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Api-Token", c.ApiToken)
+
+	queryParams := req.URL.Query()
+	queryParams.Set("pageSize", string(pageSize))
+
+	if(nextPageKey != nil) {
+		queryParams.Set("nextPageKey", *nextPageKey)
+	}
+
+	req.URL.RawQuery = queryParams.Encode()
+
+	response, err := c.Client.Do(req)
+
+	if(err != nil) {
+		return nil, err
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+	if(err != nil) {
+		return nil, err
+	}
+
+	if(response.StatusCode != 200) {
+		return nil, errors.New(fmt.Sprintf("can't retrieve extensions: reason %d %s", response.StatusCode, string(responseBody)))
+	}
+
+	var extensions GetExtensionsResponse
+	err = json.Unmarshal(responseBody, &extensions)
+	if(err != nil) {
+		return nil, err
+	}
+
+	return &extensions, nil
+
+	
+}
+
+func (c *DynatraceClient) getConfigurationApiRequest(method string, path string) (*http.Request, error) {
+	return http.NewRequest(method, fmt.Sprintf("%s%s%s", c.EnvUrl, ConfigurationApiPath, path), nil)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -5,11 +5,15 @@ package provider
 
 import (
 	"context"
+	"os"
+	"terraform-provider-dynatrace-extension/internal/dynatrace"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // Ensure ScaffoldingProvider satisfies various provider interfaces.
@@ -23,6 +27,10 @@ type DynatraceExtensionProvider struct {
 	version string
 }
 
+type DynatraceExtensionProviderModel struct {
+	EnvironmentUrl types.String `tfsdk:"envUrl"`
+	ApiToken       types.String `tfsdk:"apiToken"`
+}
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
@@ -38,10 +46,83 @@ func (p *DynatraceExtensionProvider) Metadata(ctx context.Context, req provider.
 }
 
 func (p *DynatraceExtensionProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"envUrl": schema.StringAttribute{
+				Optional: false,
+			},
+			"apiToken": schema.StringAttribute{
+				Optional: false,
+			},
+		},
+	}
 }
 
 func (p *DynatraceExtensionProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	// first checking provider configuration values
+	var config DynatraceExtensionProviderModel
+
+	// reading configuration...
+	diags := req.Config.Get(ctx, &config)
+
+	// appending all diagnostics to the response diagnostics
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		// we already have an error here, just by reading configuration, so we return here
+		return
+	}
+
+	// checking configuration values...
+	if config.ApiToken.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(path.Root("apiToken"), "Unknown API Token", "Provider cannot create the client because API Token is unknown")
+	}
+
+	if config.EnvironmentUrl.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(path.Root("envUrl"), "Unknown Environment URL", "Provider cannot create the client because Environment URL is unknown")
+	}
+
+	if resp.Diagnostics.HasError() {
+		// we already have an error here, just by reading configuration, so we return here
+		return
+	}
+
+
+	// reading environment variables and overriding with configurations if required
+	envUrl := os.Getenv("DYNATRACE_ENV_URL")
+	apiToken := os.Getenv("DYNATRACE_API_TOKEN")
+
+	if(!config.ApiToken.IsNull()) {
+		apiToken = config.ApiToken.ValueString()
+	}
+
+	if(!config.EnvironmentUrl.IsNull()) {
+		envUrl = config.EnvironmentUrl.ValueString()
+	}
+
+
+	// checking values validity
+	if envUrl == "" {
+		resp.Diagnostics.AddAttributeError(path.Root("envUrl"), "Missing Environment URL", "Provider cannot create the client because Environment URL is missing")
+	}
+
+	if apiToken == "" {
+		resp.Diagnostics.AddAttributeError(path.Root("apiToken"), "Missing API Token", "Provider cannot create the client because API Token is missing")
+	}
+
+	if resp.Diagnostics.HasError() {
+		// we already have an error here, just by reading configuration, so we return here
+		return
+	}
+
+	client, err := dynatrace.NewClient(&envUrl, &apiToken)
+	if(err != nil) {
+		resp.Diagnostics.AddError("Unable to created Dynatrace API Client", "Unexpected error :" + err.Error())
+		return
+	}
+
+	resp.DataSourceData = client
+	resp.ResourceData = client
+
 }
 
 func (p *DynatraceExtensionProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
@@ -51,6 +132,3 @@ func (p *DynatraceExtensionProvider) DataSources(ctx context.Context) []func() d
 func (p *DynatraceExtensionProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return nil
 }
-
-
-
